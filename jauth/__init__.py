ece8efad6021af7d2f90fe67ed81d15b38d3c150
Brittain Hard <brittainhard@gmail.com>
2018-03-06 18:41:07 -0600

    Put dbauth in its own module.

--- jauth/__init__.py
+++ jauth/__init__.py
@@ -1,2 +1,3 @@
 from .dict_authenticator import DictAuthenticator
 from .dummy_authenticator import DummyAuthenticator
+from .dbauth.db_authenticator import DBAuthenticator
---
+++ jauth/dbauth/db_authenticator.py
@@ -0,0 +1,23 @@
+from tornado import gen
+from jupyterhub.auth import Authenticator
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from .tables import User
+
+
+class DBAuthenticator(Authenticator):
+
+    @gen.coroutine
+    def authenticate(self, handler, data):
+        engine = create_engine('sqlite:///users.sqlite', echo=True)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        user = session.query(User).filter_by(username=data["username"]).first()
+        session.close()
+        if user:
+            if data["password"] == user.password:
+                return data['username']
+        else:
+            return None

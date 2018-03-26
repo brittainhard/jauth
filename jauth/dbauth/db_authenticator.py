d0c0ddcb74fdeb3f8ebbc041b1522d175eb63596
Brittain Hard <brittainhard@gmail.com>
2018-03-26 16:33:48 -0500

    Handle new authenticator

--- jauth/dbauth/db_authenticator.py
+++ jauth/dbauth/db_authenticator.py
@@ -11,10 +11,9 @@ class DBAuthenticator(Authenticator):
     @gen.coroutine
     def authenticate(self, handler, data):
         dbauth_url = "http://" + os.environ.get("DBAUTH_SERVICE_HOST") + ":" + os.environ.get("DBAUTH_SERVICE_PORT") + "/users/"
-        r = requests.get(dbauth_url, json={"username": data["username"]})
+        r = requests.get(dbauth_url, json={"username": data["username"], "password": data["password"]})
         user = json.loads(r.text)
         if user:
-            if data["password"] == user[0]["password"]:
-                return data['username']
+            return data['username']
         else:
             return None
